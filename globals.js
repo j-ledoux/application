6928282357f0c95db86986f58abe657bbe6d5832
j-ledoux <julien@thecamp.fr>
2016-11-04 09:28:43 +0100

    Set globals env prod by default

--- globals.js
+++ globals.js
@@ -2,7 +2,7 @@ var globals = {
     'appName': 'thecamp TimeOff',
     'domain': 'symfony.dev:3000',
     'scheme': 'http://',
-    'env': 'test'
+    'env': 'production'
 };
 
 module.exports = globals;
